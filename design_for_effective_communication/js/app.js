77c5488ad6509cedc57bc51baf35f5088ee7355d
root <root@gamma.(none)>
2014-01-31 07:07:51 +0000

    initial commit

---
+++ design_for_effective_communication/js/app.js
@@ -0,0 +1,11 @@
+$(function ()
+{
+
+  $('.step:first').width($(window).width()).height($(window).height());
+  
+  $('.step').eq(5).width($(window).width()).height($(window).height());
+  
+  $('.step').eq(7).width($(window).width()).height($(window).height());
+  impress().init();
+
+});
